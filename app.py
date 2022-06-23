ad140f4ad9ecbd639ea184ac46891739fbc64917
EricPlummerDataMgmt <eric.plummer@datamgmt.co>
2022-06-23 17:24:48 -0400

    Skeleton Code

---
+++ app.py
@@ -0,0 +1,7 @@
+import json
+
+def main():
+   print("Main Function....app.py")
+
+if __name__ == "__main__":
+    main()
---
+++ mod_2.py
@@ -0,0 +1,7 @@
+import json
+
+def main():
+   print("Main Function....app_2.py")
+
+if __name__ == "__main__":
+    main()
